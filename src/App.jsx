6c9320053f00d7c0d40b9ed103e32976e3f1f316
Matheus Silva <ms25022003@gmail.com>
2022-06-24 10:24:04 -0300

    finish Todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,48 @@ import './App.css';
 import logo from './logo.svg';
 import store from './store';
 import { useSelector } from 'react-redux'
+import { useRef, useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const { newTodo } = useSelector(state => state);
+  const refId = useRef(null)
 
-  const { newTodo } = useSelector(state => {
-    return state;
+  const createTodo = () => {
+    store.dispatch({
+      type: 'create',
+      payload: {
+        id: Math.max(...ids) + 1,
+        title: todo
+      }
+    })
+  }
+
+  const deleteTodo = (id) => {
+    store.dispatch({
+      type: 'delete',
+      payload: Number(id)
+    })
+  }
+
+  const ids = newTodo.todos.map(object => {
+    return object.id;
   });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         {newTodo.todos.map(el => (
-          <p key={el.title}>{el.title}</p>
+          <p key={el.id}>id: {el.id}<br /> title: {el.title}</p>
         ))}
-
-        <button onClick={() => store.dispatch({ type: 'create', payload: { id: 2, tile: 'Todo 2' } })}>Create todo</button>
-        <button onClick={() => store.dispatch({ type: 'delete', payload: 1 })}>Delete todo</button>
+        <div>
+          <input type="text" onChange={(e) => setTodo(e.target.value)} placeholder='title todo' />
+          <input type="number" ref={refId} placeholder='id para deletar' />
+        </div>
+        <button onClick={createTodo}>Create todo</button>
+        <button onClick={(e) => deleteTodo(refId.current.value)}>Delete todo</button>
       </header>
     </div>
   )
