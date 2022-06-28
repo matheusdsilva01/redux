a34622b2a84a5e519f2d5665a1bc6164f2ed684e
Matheus Silva <ms25022003@gmail.com>
2022-06-28 09:06:34 -0300

    thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { legacy_createStore as createStore } from "redux";
-import { combineReducers } from 'redux'
+import { combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { todo } from "./reducers/todo.js";
 
 export const store = createStore(combineReducers({
     newTodo: todo
-}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+}), applyMiddleware(thunk));
 
 // const store = createStore((
 //     (state, action) => {
